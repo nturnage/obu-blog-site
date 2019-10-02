acdcf0c9ecb439db1fc9814896a0258e814609dd
Christopher Calmes <christopher.calmes@okbu.edu>
2019-10-02 15:49:40 -0500

    Removed date from article creation form

--- resources/views/author/submit-article.blade.php
+++ resources/views/author/submit-article.blade.php
@@ -11,10 +11,6 @@
                     <input type="text" class="form-control" id="articleTitle" aria-describedby="titleHelp" placeholder="Enter Article Title">
                 </div>
                 <div class="form-group">
-                    <label for="articleDate">Date for Article</label>
-                    <input type="date" class="form-control" id="articleDate" aria-describedby="dateHelp" placeholder="{{ date('mm-dd-yyyy') }}">
-                </div>
-                <div class="form-group">
                     <label for="articleText">Article Text</label>
                     <textarea class="form-control" id="ArticleText" rows="6"></textarea>
                 </div>
